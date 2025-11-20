97d348dfb51e52c74c80d5edafddb39d722683de
mubeen <noorul.mubeen@conovoinc.com>
2025-11-20 18:55:40 +0500

    Added the empty Resposne

---
+++ Sources/AppCore/network/entity/EmptyResponse.swift
@@ -0,0 +1,9 @@
+//
+//  EmptyResponse.swift
+//  AppCore
+//
+//  Created by Noor ul Mubeen on 20/11/2025.
+//
+
+// A placeholder struct to represent an empty response body.
+public struct EmptyResponse: Decodable {}
